5da4f20c9ce75a2e8e24978d6a017e53e7190654
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-06-01 14:01:27 -0700

    get routes work on projects

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express"),
   cors = require("cors"),
   port = 5555;
 
-const projectsRouter = require("./data/routers/projectsRoutes"),
-  actionsRouter = require("./data/routers/actionsRoutes");
+const projectsRouter = require("./data/routers/projectsRoutes")
+//   actionsRouter = require("./data/routers/actionsRoutes");
 
 server.use(express.json());
 server.use(cors());
 server.use("/projects", projectsRouter);
-server.use("/actions", actionsRouter);
+// server.use("/actions", actionsRouter);
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
